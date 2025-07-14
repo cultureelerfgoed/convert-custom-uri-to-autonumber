4b49aaa0e8524db392c24976d745e3bbe6bda7ac
jonathanblok <j.s.blok@cultureelerfgoed.nl>
2025-07-14 12:39:10 +0200

    Optimized a bit by removing need for second graph and second pass, tested with TTL as input but does not work yet.

--- convert-skos-concept-to-autonumber.py
+++ convert-skos-concept-to-autonumber.py
@@ -9,7 +9,8 @@ from pathlib import Path
 autonumber_range = range(1000000, 9999999)
 
 # Defines the input file path
-input_file = "convert-skos-concept-to-autonumber/pp_project_vrouwenthesaurus.trig"
+# Currently only working for TTL..
+input_file = "convert-skos-concept-to-autonumber/pp_project_vrouwenthesaurus.ttl"
 
 # Defines the format of the output file
 output_file_format = "trig"
@@ -21,43 +22,37 @@ output_file = "convert-skos-concept-to-autonumber/vrouwenthesaurus-autonumber.tr
 base_URI = "https://vrouwenthesaurus.nl/id/"
 
 # Global variables 
-old_g = Graph()
-new_g = Graph()
+graph = Graph(identifier=base_URI+"thesaurus")
 URI_dict = {}
 id_iter = iter(autonumber_range)
 
 # Parse RDF file
 print(f"Opening file: {str(Path.as_posix(Path.cwd())) +"/"+input_file}")
-old_g.parse(str(Path.as_posix(Path.cwd())) +"/"+input_file )
-
-# Copy namespace to new graph
-for ns_prefix, ns in old_g.namespaces():
-    new_g.bind(ns_prefix, ns)
+graph.parse(str(Path.as_posix(Path.cwd())) +"/"+input_file )
+old_g_length = len(graph)
 
 # Loop through each triple in the graph (subj, pred, obj)
-for subj, pred, obj in old_g:    
+for subj, pred, obj in graph:    
     # check whether subject contains base_URI, not already in key dictionary, and of RDF.type SKOS.concept
-    if(base_URI in subj and str(subj) not in URI_dict.keys() and (URIRef(str(subj)), RDF.type, SKOS.Concept) in old_g):
+    if(base_URI in subj and str(subj) not in URI_dict.keys() and (URIRef(str(subj)), RDF.type, SKOS.Concept) in graph):
         # If so, add it to the key dictionary and increment ID iterator
         URI_dict.update({str(subj) : URIRef(base_URI + str(next(id_iter)))})        
-        new_g.add((URI_dict[str(subj)], pred, obj))
 
-    else:
-        # Otherwise it is either not a new key, or a reference, or just another triple, so add accordingly.
-        new_g.add((URI_dict.get(str(subj), subj), pred, URI_dict.get(str(obj), obj)))
+    # check whether object contains base_URI, not already in key dictionary, and of RDF.type SKOS.concept
+    if(base_URI in obj and str(obj) not in URI_dict.keys() and (URIRef(str(obj)), RDF.type, SKOS.Concept) in graph):        
+        URI_dict.update({str(obj) : URIRef(base_URI + str(next(id_iter)))})        
 
-# Second pass in case concept was mentioned as broader before concept definition
-for subj, pred, obj in new_g:    
-    if(str(subj) in URI_dict.keys() or str(obj) in URI_dict.keys()):
-        new_g.remove((subj, pred, obj))
-        new_g.add((URI_dict.get(str(subj), subj), pred, URI_dict.get(str(obj), obj)))
+    # Either add triple to graph as is, or as an updated triple.
+    graph.remove((subj, pred, obj))
+    graph.add((URI_dict.get(str(subj), subj), pred, URI_dict.get(str(obj), obj)))
 
 # Test that the new graph contains as many triples as the old graph
-assert(len(old_g) == len(new_g))
+assert(len(graph) == old_g_length)
+print(f"Graph length (new vs. old): {len(graph)} == {old_g_length}.")
 
 # Serialize new graph and write to output file
 print(f"Writing output to: {Path.as_posix(Path.cwd())+"/"+output_file}")
-new_g.serialize(format=output_file_format, destination=str(Path.as_posix(Path.cwd())+"/"+output_file))
+graph.serialize(format=output_file_format, destination=str(Path.as_posix(Path.cwd())+"/"+output_file))
 
 # Test that file wrote succesfully 
 assert(Path(output_file).exists())
